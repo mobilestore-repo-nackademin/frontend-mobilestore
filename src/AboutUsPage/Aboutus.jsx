fa08a9484c2163f9f34b415f426fceb78f67b172
marrepirre <111415251+marrepirre@users.noreply.github.com>
2024-01-26 09:47:11 +0100

    Blabla

--- src/AboutUsPage/Aboutus.jsx
+++ src/AboutUsPage/Aboutus.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import './AboutUsCss/AboutUs.css';
-import HomePage from "../Homepage/Homepage.jsx";
-import Header from "../Header.jsx";
-import Footer from "../Footer.jsx";
-import AboutUsPage from "./HeroSection.jsx";
+import HeroSection from './HeroSection.jsx';
+
 const Aboutus = () => {
     return (
         <div>
-
-            < Header/>
-            <AboutUsPage/>
-            < Footer/>
-
+         <HeroSection/>
         </div>
     );
 };
