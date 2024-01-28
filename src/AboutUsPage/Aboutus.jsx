f87bab09045ad729ed14f56264b7eb60596657bf
Taha the one <taha.taheri@yh.nackademin.se>
2024-01-28 08:28:17 +0100

    last update

--- src/AboutUsPage/Aboutus.jsx
+++ src/AboutUsPage/Aboutus.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
 import './AboutUsCss/AboutUs.css';
-
-import AboutUsPage from "./HeroSection.jsx"
-
-
-
 import AboutUsPage from "./HeroSectionAbout.jsx";
 
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import 'index.css'
+import AboutUsPage from "./AboutUsPage/HeroSectionAbout.jsx";
 
-import './index.css'
 
-<<<<<<< HEAD
-import AboutUsPage from "./AboutUsPage/HeroSection.jsx";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
     <AboutUsPage/>
 
-
-
-
-=======
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App/>
->>>>>>> 73266041f7d229f6ff62e4364acfa6c8c3327b8f
   </React.StrictMode>
 )
