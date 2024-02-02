9de540a86bf4a3f95a6174815a0ddb51f1139721
Taha the one <taha.taheri@yh.nackademin.se>
2024-02-02 20:21:43 +0100

    last

--- src/TermsAndCondition/TermsInfo.jsx
+++ src/TermsAndCondition/TermsInfo.jsx
@@ -55,29 +55,4 @@ export default PurchaseTerms;
 
 
 
-// const PurchaseTerms = () => {
-//     return (
-//         <div className= "term">
-//             <h3>Köpvillkor</h3>
-//
-//             <p>Välkommen till våra köpvillkor. Nedan finner du information om hur köp och beställningar hanteras:</p>
-//
-//             <h5>1. Beställning</h5>
-//             <p>Du kan placera en beställning genom vår webbplats. Genom att göra en beställning godkänner du våra köpvillkor och åtar dig att betala det totala beloppet för beställningen.</p>
-//
-//             <h5>2. Priser och Betalning</h5>
-//             <p>Alla priser anges i lokal valuta och inkluderar skatter. Betalning kan göras med olika betalningsmetoder som anges vid kassan. Vi förbehåller oss rätten att ändra priser och betalningsmetoder när som helst.</p>
-//
-//             <h5>3. Ångerrätt</h5>
-//             <p>Du har rätt att ångra ditt köp inom 14 dagar från mottagandet av varorna. Kontakta vår kundtjänst för att initiera returprocessen. Vänligen se våra returpolicy för ytterligare information.</p>
-//
-//             <h5>4. Leverans</h5>
-//             <p>Varaktigheten och kostnaden för leverans kan variera beroende på leveransadress och vald leveransmetod. Se våra leveransvillkor för detaljerad information.</p>
-//
-//             <h5>5. Kundtjänst</h5>
-//             <p>Vår kundtjänst står till ditt förfogande för att besvara frågor eller ge hjälp. Kontakta oss via e-post eller telefon för snabbast möjliga svar.</p>
-//         </div>
-//     );
-// };
-//
-// export default PurchaseTerms;
+
